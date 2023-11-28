3abaf080e9388407cae84a2c60cc35ba586c334c
Manas Pratim Biwas <manaspb2405@gmail.com>
2023-11-28 13:16:17 +0530

    initial react-google-maps setup

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./App.css";
+require("dotenv").config()
+
+const apiKey = process.env.GOOGLE_MAP_API_KEY;
+
+const App = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_API_KEY,
+  });
+  const center = useMemo(() => ({ lat: 22.561083, lng: 88.412665 }), []);
+
+  return (
+    <div className="App">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={18.25}
+          >
+
+        <Marker position={{ lat: 22.561083, lng: 88.412665 }} />
+        
+        </GoogleMap>
+
+        
+      )}
+    </div>
+  );
+};
+
+export default App;
