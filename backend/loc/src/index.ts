54fdc6046eeaaa3b6b8f789f256cdb331f679426
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-04-20 18:07:53 +0530

    [feat]: init loc with supabase
    - loc express server with supabase
    - postgres instance with postgis for loc querying

---
+++ backend/loc/src/index.ts
@@ -0,0 +1,24 @@
+require("dotenv").config();
+import express, { Request, Response } from "express";
+import cors from "cors";
+// import globalCatch from "./middlewares/globalCatch";
+// import pingRouter from "./routes/ping";
+
+import supabase from "./supabase";
+
+const app = express();
+const PORT: string | number = process.env.PORT || 6060;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`<pre> <i> A Privacy-Preserving Efficient Location-Sharing Scheme for Mobile Online Social Network Applications </i> 🛜 </pre>
+	<pre> ~ Built with &#x1F499 by sanam </pre>`);
+});
+
+supabase().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is listening at port no", PORT);
+  });
+});
